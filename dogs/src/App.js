d5eb8e2613d7af67188ed308e60b1714197dddf8
WesleyRabachiniRibeiro <wesleyrr2015@gmail.com>
2023-02-04 16:18:30 -0300

    created the Header, project structure and add images

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,7 +1,21 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import styles from "./App.css"
+
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/login" element={<Login />}/>
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
